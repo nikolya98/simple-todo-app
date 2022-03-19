145ee92fd8a5e13be02bbf9ad1235726ba73e4d8
nikolya98 <nikolya.knyazev.98@mail.ru>
2022-03-19 23:40:35 +0300

    feat: add TaskListControl component
    TaskListControl is a component for managing the display of the task list.

---
+++ src/App/components/TaskListControl/TaskListControl.jsx
@@ -0,0 +1,65 @@
+import { useCallback, useState } from "react";
+
+import Button from "@components/Button";
+import { getActiveTasks, getCompletedTasks } from "@utils/filters";
+
+import TasksList from "../TasksList";
+import style from "./TaskListControl.module.scss";
+
+const TaskListControl = ({ tasks, onClear, onDelete, onEdit }) => {
+  const [group, setGroup] = useState("all");
+  let filteredTasks;
+
+  switch (group) {
+    case "active":
+      filteredTasks = getActiveTasks(tasks);
+      break;
+    case "completed":
+      filteredTasks = getCompletedTasks(tasks);
+      break;
+    default:
+      filteredTasks = tasks;
+  }
+
+  const setAll = useCallback(() => setGroup("all"), [setGroup]);
+  const setActive = useCallback(() => setGroup("active"), [setGroup]);
+  const setCompleted = useCallback(() => setGroup("completed"), [setGroup]);
+  const handleClear = useCallback(() => {
+    onClear();
+    setActive("all");
+  }, [onClear, setActive]);
+
+  return (
+    <>
+      <TasksList tasks={filteredTasks} onDelete={onDelete} onEdit={onEdit} />
+      <div className={style.wrapper}>
+        <span className={style.info}>
+          {getActiveTasks(tasks).length} tasks left
+        </span>
+        <Button className={`${style.button} ${style.all}`} onClick={setAll}>
+          All
+        </Button>
+        <Button
+          className={`${style.button} ${style.active}`}
+          onClick={setActive}
+        >
+          Active
+        </Button>
+        <Button
+          className={`${style.button} ${style.completed}`}
+          onClick={setCompleted}
+        >
+          Completed
+        </Button>
+        <Button
+          className={`${style.button} ${style.clear}`}
+          onClick={handleClear}
+        >
+          Clear
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default TaskListControl;
